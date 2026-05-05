42edfec5abd304bb24a1942d331c177bcf8605dd
Gagandeep Singh <singh.gagandeepgs@gmail.com>
2026-05-05 13:55:55 +0530

    Implemented TestNG Test-Runner for regression xml with browser name parameterized

--- src/test/java/com/qa/opencart/base/BaseTest.java
+++ src/test/java/com/qa/opencart/base/BaseTest.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 import com.qa.opencart.factory.DriverFactory;
 import com.qa.opencart.pages.AccountsPage;
@@ -25,10 +26,16 @@ public class BaseTest {
 	protected ProductInfoPage productInfoPage;
 	
 	
+	@Parameters({"browser"})
 	@BeforeTest
-	public void setup() {
+	public void setup(String browserName) {
 		df = new DriverFactory();
 		prop = df.initProperties();
+		
+		if(browserName != null) {
+			prop.setProperty("browser", browserName);
+		}
+		
 		driver = df.initDriver(prop);
 		loginPage = new LoginPage(driver);
 	}
