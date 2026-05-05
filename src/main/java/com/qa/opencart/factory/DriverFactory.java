2c845d1549e21b7f6e85925c4659d91387e04cc6
Gagandeep Singh <singh.gagandeepgs@gmail.com>
2026-05-05 14:21:18 +0530

    Implmented Thread-local concept for WebDriver Synchronization

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -18,6 +18,7 @@ public class DriverFactory {
 	
 	private WebDriver driver;
 	private Properties prop;
+	private static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>(); // Why static???
 	
 	public WebDriver initDriver(Properties properties) {
 		String browser = properties.getProperty("browser");
@@ -25,27 +26,35 @@ public class DriverFactory {
 		
 		switch (browser.toLowerCase().trim()) {
 		case "chrome":
-			driver = new ChromeDriver();
+			//driver = new ChromeDriver();
+			tlDriver.set(new ChromeDriver());
 			break;
 		case "firefox":
-			driver = new FirefoxDriver();
+			//driver = new FirefoxDriver();
+			tlDriver.set(new FirefoxDriver());
 			break;
 		case "edge":
-			driver = new EdgeDriver();
+			//driver = new EdgeDriver();
+			tlDriver.set(new EdgeDriver());
 			break;
 		case "safari":
-			driver = new SafariDriver();
+			//driver = new SafariDriver();
+			tlDriver.set(new SafariDriver());
 			break;
 		default:
 			System.out.println(browser + AppErrors.INVALID_BROWSER_MESSAGE);
 			throw new FrameworkExceptions("===INVALID BROWSER===");
 		}
 		
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		driver.get(properties.getProperty("url"));
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		getDriver().get(properties.getProperty("url"));
 		
-		return driver;
+		return getDriver();
+	}
+	
+	private WebDriver getDriver() {
+		return tlDriver.get();
 	}
 	
 	public Properties initProperties() {
